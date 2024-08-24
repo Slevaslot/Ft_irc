a965886b80bb2aaddfa4f003387738edb3481933
Souleimane LEVASLOT <slevaslo@made-f0Ar3s11.clusters.42paris.fr>
2024-08-24 22:20:15 +0200

    clear

--- cmds/Nick.cpp
+++
@@ -1,7 +0,0 @@
-#include "../includes/cmds.hpp"
-
-void	nick(std::string newNickname, Client currentClient)
-{
-	currentClient.setNickname(newNickname);
-	std::cout << "currentClient : " << currentClient.GetNickname() << std::endl;
-}
--- includes/cmds.hpp
+++
@@ -1,7 +0,0 @@
-#include "server.hpp"
-
-// void	ping(std::vector<std::string> cmdSplited, int fd, std::string nickName);
-void	pass(std::string pass, std::string rightPass, Client &currentClient);
-// void	join(std::vector<std::string> cmdSplited, int fd, Client &currentClient);
-void	user(std::string newUsername, Client currClient);
-void	nick(std::string newnickname, Client currClient);
