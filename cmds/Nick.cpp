fb86257023ffb6eb0c9be7755f44fc444c407790
Souleimane LEVASLOT <slevaslo@made-f0Ar9s8.clusters.42paris.fr>
2024-10-30 16:49:57 +0100

    clear

--- cmds/Nick.cpp
+++
@@ -1,7 +0,0 @@
-#include "../includes/irc.hpp"
-
-void nick(std::string newNickname, Client currentClient)
-{
-	currentClient.setNickname(newNickname);
-	std::cout << "currentClient : " << currentClient.GetNickname() << std::endl;
-}
--- cmds/User.cpp
+++
@@ -1,8 +0,0 @@
-#include "../includes/irc.hpp"
-
-void user(std::string newUsername, Client currentClient)
-{
-	currentClient.setUsername(newUsername);
-	std::cout << "currentClient : " << currentClient.GetNickname() << std::endl;
-	std::cout << "USER command received with username: " << newUsername << std::endl;
-}
